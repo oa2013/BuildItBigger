440e715c72fc4e46763388829a3d480228c11ac4
OA <miohoya@gmail.com>
2018-09-10 20:39:22 -0400

    Feat: added a test class for the async task

---
+++ app/src/androidTest/java/AsyncTaskAndroidTest.java
@@ -0,0 +1,51 @@
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
+
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import java.util.concurrent.CountDownLatch;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Olga Agafonova on 9/10/18.
+ *
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class AsyncTaskAndroidTest{
+
+    final CountDownLatch signal = new CountDownLatch(1);
+    Context context;
+
+    /*
+    * Method based on https://stackoverflow.com/questions/2321829/android-asynctask-testing-with-android-test-framework
+    * */
+    @Test
+    public void testAsyncTask() throws Throwable{
+        assertTrue(true);
+        final CountDownLatch latch = new CountDownLatch(1);
+        context = InstrumentationRegistry.getContext();
+
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask() {
+            @Override
+            protected void onPostExecute(String joke) {
+                assertNotNull(joke);
+                if (joke != null){
+                    assertTrue(joke.length() > 0);
+                    latch.countDown();
+                }
+            }
+        };
+        endpointsAsyncTask.execute(new Pair<Context, String>(context, "dummyInput"));
+        latch.await();
+
+    }
+
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Pair;
 import android.widget.Toast;
-
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -31,12 +30,10 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
 
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
-        if(myApiService == null) {  // Only do this once
+
+        if(myApiService == null) {
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
                     .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
@@ -44,7 +41,6 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
                     });
-            // end options for devappserver
 
             myApiService = builder.build();
         }
